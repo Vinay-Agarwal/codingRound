84f806024c210ab157ab172735d40397cb6a6b4e
Vinay-Agarwal <vkkumar1994@gmail.com>
2019-03-04 14:53:25 +0530

    Update HotelBookingTest.java
    Added Selecting Location from autocomplete Location List

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -1,13 +1,22 @@
+package main.java;
+
 import com.sun.javafx.PlatformUtil;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class HotelBookingTest {
-     @FindBy(xpath= "//*[@class='hotelApp ']")
+	WebDriver driver;
+
+    @FindBy(xpath= "//*[@class='hotelApp ']")
     public WebElement hotelLink;
     
     @FindBy(xpath = "//*[@id='Tags']")
@@ -18,11 +27,17 @@ public class HotelBookingTest {
 
     @FindBy(id = "travellersOnhome")
     private WebElement travellerSelection;
+    
+    @FindBy(xpath= "//*[@class='ui-state-default ui-state-highlight ']")
+    public WebElement CheckInDateSelector;
 
+    @FindBy(xpath = "//*[@class='ui-state-default ']")
+    public List<WebElement> CheckoutDateSelctor;
+    
     public void HotelBookingTest(WebDriver driver) {
         PageFactory.initElements(driver, this);
     }
-    
+
     @Test
     public void shouldBeAbleToSearchForHotels() {
         setDriverPath();
@@ -30,21 +45,41 @@ public class HotelBookingTest {
         HotelBookingTest(driver);
         driver.get("https://www.cleartrip.com/");
         hotelLink.click();
-        localityTextBox.sendKeys("Indiranagar, Bangalore");
-
-        new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
+       	localityTextBox.sendKeys("Indiranagar, Bangalore");	
+       	waitFor(4000);
+       	boolean LocationListDisplayed = driver.findElement(By.xpath("//*[@class='list']")).isDisplayed();
+       	System.out.println("Location list displayed: " + LocationListDisplayed);
+       	List<WebElement> LocationList = driver.findElements(By.xpath("//*[@class='list']"));
+       	if(LocationListDisplayed) {
+       		LocationList.get(0).click();
+       	}
+       	
+       	//Selecting current date as start date and +1 for End Date
+       	CheckInDateSelector.click();
+       	CheckoutDateSelctor.get(0).click();
+       	
+       	
+        new Select(travellerSelection).selectByVisibleText("2 rooms, 4 adults");
         searchButton.click();
-
-        driver.quit();
-
+      //  driver.quit();
+        
     }
+    
+    private void waitFor(int durationInMilliSeconds) {
+        try {
+            Thread.sleep(durationInMilliSeconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+    
 
-    private void setDriverPath() {
+	private void setDriverPath() {
         if (PlatformUtil.isMac()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver");
         }
         if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        	System.setProperty("webdriver.chrome.driver", "C:\\Users\\vinay.kumar.agarwal\\Downloads\\chromedriver_win32_Latest\\chromedriver.exe");
         }
         if (PlatformUtil.isLinux()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
