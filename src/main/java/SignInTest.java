9e7ad308f092dd54d4278735bf99d7811ee0103f
Vinay-Agarwal <vkkumar1994@gmail.com>
2019-03-05 19:06:55 +0530

    Update SignInTest.java

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -1,68 +1,47 @@
+package main.java;
+
 import com.sun.javafx.PlatformUtil;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SignInTest {
+	WebDriver driver;
 
-    
+  //  WebDriver driver = new ChromeDriver(); // Moved this line from line 12 to Line 18 after setting up Driver Path
 
-    @Test
+    @SuppressWarnings("deprecation")
+	@Test
     public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
-
-        setDriverPath();
-        WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, 20);
+    	BasePage basePageObj = new BasePage();
+    	driver=basePageObj.setDriverPath();        
         driver.get("https://www.cleartrip.com/");
-        waitFor(2000);
-
+        basePageObj.waitFor(2000);
         driver.manage().window().maximize();
         WebElement yourTrip = driver.findElement(By.linkText("Your trips")); 
         WebElement SignIn = driver.findElement(By.id("SignIn"));
-        
-        clickElement(yourTrip);
-        clickElement(SignIn);
-
+        basePageObj.clickElement(yourTrip);
+        basePageObj.clickElement(SignIn);
         driver.switchTo().frame("modal_window");
     	WebElement signInButton = driver.findElement(By.id("signInButton"));
-        clickElement(signInButton);
+    	// WebElement SignInButton = driver.findElement(By.id("signInButton"));
+    	basePageObj.clickElement(signInButton);
+
 
         String errors1 = driver.findElement(By.id("errors1")).getText();
         Assert.assertTrue(errors1.contains("There were errors in your submission"));
         driver.quit();
     }
-
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
     
-    private boolean ExplicitWait(WebElement elem) {
-    	WebDriverWait wait = new WebDriverWait(driver, 20);
-    	boolean elemExist = wait.until(ExpectedConditions.invisibilityOfElementLocated((By) elem));
-		return elemExist;
-    }
-    
-    private void clickElement(WebElement elem) {
-    	elem.click();
-    }
-
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
-
-
 }
